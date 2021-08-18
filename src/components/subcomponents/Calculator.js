4f8e1893599a5aa0f7d5732a41d4988b975e4ed4
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2021-08-18 20:26:56 +0200

    small bug fix

--- src/components/subcomponents/Calculator.js
+++ src/components/subcomponents/Calculator.js
@@ -1,43 +1,48 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react'
 
 
 export default function Calculator(props) {
-    const [inp1, setInp1]=useState('')
-    const [inp2, setInp2]=useState('')
+    const [inp1, setInp1] = useState('')
+    const [inp2, setInp2] = useState('')
 
     const changeInp1 = (e) => {
-        if(e.target.value === ''){
+        if (e.target.value === '') {
             setInp1(e.target.value);
             setInp2(e.target.value);
         }
-        else{
+        else {
             setInp1(e.target.value);
             setInp2((e.target.value * props.price).toFixed(4));
         }
-      }
-    
-      const changeInp2 = (e) => {
-        if(e.target.value === ''){
+    }
+
+    const changeInp2 = (e) => {
+        if (e.target.value === '') {
             setInp1(e.target.value);
             setInp2(e.target.value);
         }
-        else{
+        else {
             setInp2(e.target.value);
             setInp1((e.target.value / props.price).toFixed(8));
-        }  
-      }
+        }
+    }
     return (
-        <div className="section calc p-l-15 p-r-15">
-            <p className="fs-14 m-t-8 p-l-8 m-b-8">Calculator:</p>
-            <div className="calcinp ">
-                <label className="ab-l-m m-l-15">{props.symbol}:</label>
-                <input type="number" value={inp1} placeholder="0" onChange={changeInp1} className="tokeninp m-b-15"></input>
+        <div className="section calc nopad">
+            <div className="fs-14 p-t-15 p-b-15 p-l-15 m-b-10 bor">
+                Calculator:
             </div>
-            <br/>
-            <div className="calcinp m-b-15">
-                <label className="ab-l-m m-l-15">USD:</label>
-                <input type="number" value={inp2} placeholder="0" onChange={changeInp2} className="tokeninp m-b-15"></input>
+            <div className="p-l-15 p-r-15">
+                <div className="calcinp ">
+                    <label className="ab-l-m m-l-15">{props.symbol}:</label>
+                    <input type="number" value={inp1} placeholder="0" onChange={changeInp1} className="tokeninp m-b-15"></input>
+                </div>
+                <br />
+                <div className="calcinp m-b-15">
+                    <label className="ab-l-m m-l-15">USD:</label>
+                    <input type="number" value={inp2} placeholder="0" onChange={changeInp2} className="tokeninp m-b-15"></input>
+                </div>
             </div>
+
         </div>
     )
 }

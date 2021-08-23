b0fa4749811f3c4470349344b086bd04200013f7
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2021-08-23 11:28:35 +0200

    fixed chart color bug

--- src/Tokens.js
+++ src/Tokens.js
@@ -100,7 +100,7 @@ export default class Tokens extends React.Component {
           if (tokens[i].histData[0] > tokens[i].histData[tokens[i].histData.length - 1]) {
             chartCol[i] = '#16c784'
           }
-          else if (tokens[i].histData[0] > tokens[i].histData[tokens[i].histData.length - 1]) {
+          else if (tokens[i].histData[0] < tokens[i].histData[tokens[i].histData.length - 1]) {
             chartCol[i] = '#ea3943'
           }
           else {
@@ -114,53 +114,55 @@ export default class Tokens extends React.Component {
       for (let i = 0; i < tokens.length; i++) {
         let link = `/tokens/${tokens[i].symbol.replace(/\s+/g, '_')}`
         table.push(
-            <tr key={i} className="table-row">
-              <Td cn="numtd trans-0-2 psnu" to={link}>
-                <span className="txt-l fw-400 t-lg fs-14 ">{i + 1}</span>
-              </Td>
-              <Td cn="logotd trans-0-2 pslo" to={link}>
-                <img alt={tokens[i].name} className="tokenlogo ab-c-m" src={tokens[i].logo} />
-              </Td>
-              <Td cn="psnam trans-0-2" to={link}>
-                <span className="fs-14">{tokens[i].name}</span>
-                <br />
-                <span className="fs-12 t-s fw-400">{tokens[i].symbol}<span className="pd fs-12 t-s fw-400"> - Tvl: ${abbreviateNumber(tokens[i].tvl)}</span></span>
-              </Td>
-              <Td to={link} cn="txt-r charttdd fw-400 fs-14 pd ">
-                <Smallchart histData={every_nth(histData[i], 4)} weekChange={tokens[i].weekChange} col={chartCol[i]} />
-              </Td>
-              <Td to={link} cn="txt-r pricetd fs-14">
-                <span>${parseFloat(tokens[i].price).toPrecision(4)}</span>
-                <br className="pdd" />
-                <span className={"txt-r pctd fw-400 fs-12 pd " + dayCol[i]}>{parseFloat(tokens[i].dayChange).toFixed(2)}%</span>
-              </Td>
-              <Td to={link} cn={"txt-r pctd pdn "}><span className={"fw-400 fs-14 pdn "+ dayCol[i]}>{parseFloat(tokens[i].dayChange).toFixed(2)}%</span></Td>
-              <Td to={link} cn={"txt-r pctd pdn "}><span className={"fw-400 fs-14 pdn "+ weekCol[i]}>{parseFloat(tokens[i].weekChange).toFixed(2)}%</span></Td>
-              <Td to={link} cn="txt-r pricetd pdn "><span className="fw-400 fs-14 pdn ">${numberWithCommas(tokens[i].tvl.toFixed(2))}</span></Td>
-              <Td to={link} cn="txt-r suptd pdn "><span className="fw-400 fs-14 pdn ">{abbreviateNumber(tokens[i].supply)}</span></Td>
-              <Td to={link} cn="txt-r charttd pdn "><Smallchart histData={every_nth(histData[i], 3)} weekChange={tokens[i].weekChange} col={chartCol[i]} /></Td>
-            </tr>
-          
+          <tr key={i} className="table-row">
+            <Td cn="numtd trans-0-2 psnu" to={link}>
+              <span className="txt-l fw-400 t-lg fs-14 ">{i + 1}</span>
+            </Td>
+            <Td cn="logotd trans-0-2 pslo" to={link}>
+              <img alt={tokens[i].name} className="tokenlogo ab-c-m" src={tokens[i].logo} />
+            </Td>
+            <Td cn="psnam trans-0-2" to={link}>
+              <span className="fs-14">{tokens[i].name}</span>
+              <br />
+              <span className="fs-12 t-s fw-400">{tokens[i].symbol}<span className="pd fs-12 t-s fw-400"> - Tvl: ${abbreviateNumber(tokens[i].tvl)}</span></span>
+            </Td>
+            <Td to={link} cn="txt-r charttdd fw-400 fs-14 pd ">
+              <Smallchart histData={every_nth(histData[i], 4)} weekChange={tokens[i].weekChange} col={chartCol[i]} />
+            </Td>
+            <Td to={link} cn="txt-r pricetd fs-14">
+              <span>${parseFloat(tokens[i].price).toPrecision(4)}</span>
+              <br className="pdd" />
+              <span className={"txt-r pctd fw-400 fs-12 pd " + dayCol[i]}>{parseFloat(tokens[i].dayChange).toFixed(2)}%</span>
+            </Td>
+            <Td to={link} cn={"txt-r pctd pdn "}><span className={"fw-400 fs-14 pdn " + dayCol[i]}>{parseFloat(tokens[i].dayChange).toFixed(2)}%</span></Td>
+            <Td to={link} cn={"txt-r pctd pdn "}><span className={"fw-400 fs-14 pdn " + weekCol[i]}>{parseFloat(tokens[i].weekChange).toFixed(2)}%</span></Td>
+            <Td to={link} cn="txt-r pricetd pdn "><span className="fw-400 fs-14 pdn ">${numberWithCommas(tokens[i].tvl.toFixed(2))}</span></Td>
+            <Td to={link} cn="txt-r suptd pdn "><span className="fw-400 fs-14 pdn ">{abbreviateNumber(tokens[i].supply)}</span></Td>
+            <Td to={link} cn="txt-r charttd pdn "><Smallchart histData={every_nth(histData[i], 3)} weekChange={tokens[i].weekChange} col={chartCol[i]} /></Td>
+          </tr>
+
         )
       }
       return (
-        <table className="w-full" id="table">
-          <thead className="">
-            <tr className="">
-              <th className="txt-l fs-12 c-ab pdn psnu">#</th>
-              <th className="txt-l fs-12 c-ab pdn psna" colSpan="2">Name</th>
-              <th className="txt-r fs-12 c-ab pdn" colSpan="2">Price</th>
-              <th className="txt-r fs-12 c-ab pdn">24h%</th>
-              <th className="txt-r fs-12 c-ab pdn">7d%</th>
-              <th className="txt-r fs-12 c-ab pdn">TVL</th>
-              <th className="txt-r fs-12 c-ab pdn">Supply</th>
-              <th className="txt-r fs-12 c-ab pdn">7d Chart</th>
-            </tr>
-          </thead>
-          <tbody>
-            {table}
-          </tbody>
-        </table>
+        <div>
+          <table className="w-full" id="table">
+            <thead className="">
+              <tr className="">
+                <th className="txt-l fs-12 c-ab pdn psnu">#</th>
+                <th className="txt-l fs-12 c-ab pdn psna" colSpan="2">Name</th>
+                <th className="txt-r fs-12 c-ab pdn" colSpan="2">Price</th>
+                <th className="txt-r fs-12 c-ab pdn">24h%</th>
+                <th className="txt-r fs-12 c-ab pdn">7d%</th>
+                <th className="txt-r fs-12 c-ab pdn">TVL</th>
+                <th className="txt-r fs-12 c-ab pdn">Supply</th>
+                <th className="txt-r fs-12 c-ab pdn">7d Chart</th>
+              </tr>
+            </thead>
+            <tbody>
+              {table}
+            </tbody>
+          </table>
+        </div>
       )
     }
   }

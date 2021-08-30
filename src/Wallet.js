dbe6f83b2351de63a888d308e4cecf74c4d37791
dimka3553 <37512400+dimka3553@users.noreply.github.com>
2021-08-30 16:58:25 +0300

    slight cosmetic buff

--- src/Wallet.js
+++ src/Wallet.js
@@ -19,9 +19,9 @@ export default function Wallet() {
                 <br />
                 <br />
                 <div className="inputsdiv">
-                    <input value={inp} onChange={changeInp} className="walletinp m-r-10" placeholder="Input your wallet" />
+                    <input value={inp} onChange={changeInp} className="walletinp m-r-10 fs-16" placeholder="Input your wallet" />
                     <Link to={`/wallet/${inp}`}>
-                        <button className="walletbtn fs-14">View wallet</button>
+                        <button className="walletbtn fs-16 c-pointer">View wallet</button>
                     </Link>
                 </div>
 

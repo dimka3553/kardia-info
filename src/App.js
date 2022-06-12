3d3b2beba9ccbeacd56d7a0e0726819b76294bbb
smoothM <37512400+dimka3553@users.noreply.github.com>
2022-06-12 11:54:44 +0100

    gINFO

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Tokens from './Tokens';
-import Tokenpage from './components/Tokenpage';
-import Home from './Home';
-import Lps from './Lps';
-import Wallet from './Wallet';
-import Walletdash from './components/Walletdash'
-import Airdrop from './Airdrop';
-import Airdropper from './Airdropper';
-import Clubhouse from './Clubhouse';
-import Game from './Game';
-import Infogame from './Infogame';
-import Farm from './Farm';
-import './styles/utils.css';
-import './styles/styles.css';
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Tokens from "./Tokens";
+import Tokenpage from "./components/Tokenpage";
+import Home from "./Home";
+import Lps from "./Lps";
+import Wallet from "./Wallet";
+import Walletdash from "./components/Walletdash";
+import Airdrop from "./Airdrop";
+import Airdropper from "./Airdropper";
+import Clubhouse from "./Clubhouse";
+import Game from "./Game";
+import Infogame from "./Infogame";
+import Farm from "./Farm";
+import "./styles/utils.css";
+import "./styles/styles.css";
 
 function App() {
   return (
@@ -23,6 +23,40 @@ function App() {
         <Navbar />
         <Sidebar />
         <div className="content">
+          <div className="alerttt p-t-20 p-b-20 p-l-20 p-r-20 m-l-20 m-r-20 m-t-20 m-b-20 pos-r">
+            Dear INFO holders,
+            <br />
+            <br />
+            Kardia info will soon launch gINFO which will (in theory) have a
+            positive impact in INFO's price. You may learn more about it by
+            watching the following video:
+            <br />
+            <br />
+            <a href="https://youtu.be/s2WERuqX080" target="_blank">
+              https://youtu.be/s2WERuqX080
+            </a>
+            <br />
+            <br />
+            After watching the video please vote for gINFO here:
+            <br />
+            <br />
+            <a href="https://bit.ly/3Hc0j58" target="_blank">
+              https://bit.ly/3Hc0j58
+            </a>
+            <br />
+            <br />
+            Thank you very much for your support,
+            <br />
+            <br />
+            INFO team.
+            <br />
+            <br />
+            P.S. Once you vote for gINFO please DM{" "}
+            <a href="https://t.me/dima3553" target="_blank">
+              @dima3553
+            </a>{" "}
+            on Telegram to get a little present.
+          </div>
           <Switch>
             <Route exact path="/">
               <Home />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,74 +1,76 @@
-import React, { Component } from 'react'
-import Hamburger from './subcomponents/Hamburger'
-import Sidelink from './subcomponents/Sidelink'
-import Homesvg from './subcomponents/svgs/Homesvg'
-import Tokensvg from './subcomponents/svgs/Tokensvg'
-import Walletsvg from './subcomponents/svgs/Walletsvg'
-import Gamesvg from './subcomponents/svgs/Gamesvg'
-import Drop from './subcomponents/svgs/Drop'
-import Piggy from './subcomponents/svgs/Piggy'
-import Farmsvg from './subcomponents/svgs/Farmsvg'
+import React, { Component } from "react";
+import Hamburger from "./subcomponents/Hamburger";
+import Sidelink from "./subcomponents/Sidelink";
+import Homesvg from "./subcomponents/svgs/Homesvg";
+import Tokensvg from "./subcomponents/svgs/Tokensvg";
+import Walletsvg from "./subcomponents/svgs/Walletsvg";
+import Gamesvg from "./subcomponents/svgs/Gamesvg";
+import Drop from "./subcomponents/svgs/Drop";
+import Piggy from "./subcomponents/svgs/Piggy";
+import Farmsvg from "./subcomponents/svgs/Farmsvg";
 
 export default class Sidebar extends Component {
-    render() {
-        return (
-            <div id="sidebarWrapper noselect">
-                <div id="sidebar">
-                    <div className="sidelinks m-t-6">
-                        <Sidelink name="Overview" cName="overview" lnk="/">
-                            <Homesvg/>
-                        </Sidelink>
-                        <Sidelink name="Stake" cName="drop" lnk="/club">
-                            <Piggy />
-                        </Sidelink>
-                        <Sidelink name="Farm" cName="drop" lnk="/farm">
-                            <Farmsvg />
-                        </Sidelink>
-                        <Sidelink name="Win KAI" cName="game" lnk="/game">
-                            <Gamesvg />
-                        </Sidelink>
-                        <Sidelink name="Tokens" cName="tokens" lnk="/tokens">
-                            <Tokensvg/>
-                        </Sidelink>
-                        <Sidelink name="Wallet" cName="wallet" lnk="/wallet">
-                            <Walletsvg/>
-                        </Sidelink>
-                        <Sidelink name="Airdrop" cName="drop" lnk="/airdrop">
-                            <Drop />
-                        </Sidelink>
-                    </div>
-                </div>
-                <div id="smallsidebar">
-                    <div className="sidebar-nav trans-0-2 pos-r">
-                        <Hamburger/>
-                        <span className="smallsidebar-menu t-g ab-l-m m-l-51 fs-20 noselect">Menu</span>
-                    </div>
-                    <div className="sidelinks m-t-15 p-l-15 p-r-15">
-                        <Sidelink name="Overview" cName="overview" lnk="/">
-                            <Homesvg/>
-                        </Sidelink>
-                        <Sidelink name="Stake" cName="drop" lnk="/club">
-                            <Piggy />
-                        </Sidelink>
-                        <Sidelink name="Farm" cName="drop" lnk="/farm">
-                            <Farmsvg />
-                        </Sidelink>
-                        <Sidelink name="Win KAI" cName="game" lnk="/game">
-                            <Gamesvg />
-                        </Sidelink>
-                        <Sidelink name="Tokens" cName="tokens" lnk="/tokens">
-                            <Tokensvg/>
-                        </Sidelink>
-                        <Sidelink name="Wallet" cName="wallet" lnk="/wallet">
-                            <Walletsvg/>
-                        </Sidelink>
-                        <Sidelink name="Airdrop" cName="drop" lnk="/airdrop">
-                            <Drop />
-                        </Sidelink>
-                    </div>
-                </div>
-                <div className="sidebar-overlay hamb-menu"></div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="sidebarWrapper noselect">
+        <div id="sidebar">
+          <div className="sidelinks m-t-6">
+            <Sidelink name="Overview" cName="overview" lnk="/">
+              <Homesvg />
+            </Sidelink>
+            <Sidelink name="Stake" cName="drop" lnk="/club">
+              <Piggy />
+            </Sidelink>
+            <Sidelink name="Farm" cName="drop" lnk="/farm">
+              <Farmsvg />
+            </Sidelink>
+            <Sidelink name="Game" cName="game" lnk="/game">
+              <Gamesvg />
+            </Sidelink>
+            <Sidelink name="Tokens" cName="tokens" lnk="/tokens">
+              <Tokensvg />
+            </Sidelink>
+            <Sidelink name="Wallet" cName="wallet" lnk="/wallet">
+              <Walletsvg />
+            </Sidelink>
+            <Sidelink name="Airdrop" cName="drop" lnk="/airdrop">
+              <Drop />
+            </Sidelink>
+          </div>
+        </div>
+        <div id="smallsidebar">
+          <div className="sidebar-nav trans-0-2 pos-r">
+            <Hamburger />
+            <span className="smallsidebar-menu t-g ab-l-m m-l-51 fs-20 noselect">
+              Menu
+            </span>
+          </div>
+          <div className="sidelinks m-t-15 p-l-15 p-r-15">
+            <Sidelink name="Overview" cName="overview" lnk="/">
+              <Homesvg />
+            </Sidelink>
+            <Sidelink name="Stake" cName="drop" lnk="/club">
+              <Piggy />
+            </Sidelink>
+            <Sidelink name="Farm" cName="drop" lnk="/farm">
+              <Farmsvg />
+            </Sidelink>
+            <Sidelink name="Game" cName="game" lnk="/game">
+              <Gamesvg />
+            </Sidelink>
+            <Sidelink name="Tokens" cName="tokens" lnk="/tokens">
+              <Tokensvg />
+            </Sidelink>
+            <Sidelink name="Wallet" cName="wallet" lnk="/wallet">
+              <Walletsvg />
+            </Sidelink>
+            <Sidelink name="Airdrop" cName="drop" lnk="/airdrop">
+              <Drop />
+            </Sidelink>
+          </div>
+        </div>
+        <div className="sidebar-overlay hamb-menu nav"></div>
+      </div>
+    );
+  }
 }
